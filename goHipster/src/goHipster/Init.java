e5a43cc7cba743513dd66ef583117c1f3fbbb583
21438581 <21438581@N204-18.alumnos.uem.es>
2015-10-20 11:39:19 +0200

    prueba

--- goHipster/src/goHipster/Init.java
+++ goHipster/src/goHipster/Init.java
@@ -3,7 +3,7 @@ package goHipster;
 public class Init {
 	public static void main(String[] args) 
 	{
-		System.out.println("Daniel Gonzalez Castillo");
+		System.out.println("Daniel Gonzalez Castillo asdfgbhfghgfhgfhfghgf");
 		
 	}
 
