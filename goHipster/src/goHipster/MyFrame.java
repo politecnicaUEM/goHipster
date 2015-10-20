1aa18eb687f1d177b04341beac3988146254ed39
21438255 <21438255@N204-05.alumnos.uem.es>
2015-10-20 13:38:01 +0200

    Final version

--- goHipster/src/goHipster/MyFrame.java
+++ goHipster/src/goHipster/MyFrame.java
@@ -43,6 +43,7 @@ public class MyFrame extends JFrame {
 				try {
 					MyFrame frame = new MyFrame();
 					frame.setVisible(true);
+					frame.setTitle("go hipster go!");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -55,7 +56,7 @@ public class MyFrame extends JFrame {
 	 */
 	public MyFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 300);
+		setBounds(100, 100, 500, 380);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -84,6 +85,9 @@ public class MyFrame extends JFrame {
 		JCheckBox chckbxNewCheckBox_3 = new JCheckBox("bot\u00F3n cuello");
 		panel_4.add(chckbxNewCheckBox_3);
 		
+		Component verticalGlue = Box.createVerticalGlue();
+		panel_4.add(verticalGlue);
+		
 		Component horizontalGlue = Box.createHorizontalGlue();
 		panel.add(horizontalGlue);
 		
@@ -111,21 +115,30 @@ public class MyFrame extends JFrame {
 		grupoBoton.add(rdbtnNewRadioButton_1);
 		grupoBoton.add(rdbtnNewRadioButton_2);
 		
+		Component verticalGlue_1 = Box.createVerticalGlue();
+		panel_7.add(verticalGlue_1);
+		
+		Component verticalStrut = Box.createVerticalStrut(10);
+		panel_5.add(verticalStrut);
+		
 		JPanel panel_6 = new JPanel();
 		panel_5.add(panel_6);
 		panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.X_AXIS));
 		
 		JButton btnNewButton = new JButton("");
-		btnNewButton.setMargin(new Insets(0, 0, 0, 0));
+		btnNewButton.setMargin(new Insets(1, 1, 1, 1));
 		panel_6.add(btnNewButton);
 		btnNewButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		btnNewButton.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/Annoying-Hipster@Low.png")));
+		btnNewButton.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/Annoying-Hipster.png")));
 		
 		JButton btnNewButton_1 = new JButton("");
-		btnNewButton_1.setMargin(new Insets(0, 0, 0, 0));
+		btnNewButton_1.setMargin(new Insets(1, 1, 1, 1));
 		panel_6.add(btnNewButton_1);
 		btnNewButton_1.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnNewButton_1.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/Male-User@Low.png")));
+		btnNewButton_1.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/Male-User.png")));
+		
+		Component rigidArea = Box.createRigidArea(new Dimension(20, 20));
+		panel_5.add(rigidArea);
 		
 		JPanel panel_1 = new JPanel();
 		contentPane.add(panel_1);
