93f415d96666d4805f1c9b203096ca340771b91c
Felipe Andrés Vásquez Tort <felipevasquez.tort@gmail.com>
2015-10-24 14:00:13 +0200

    Se ha agregado una nueva clase donde se encontrara la parte ejecutable del projecto.

---
+++ goHipster/src/paquete_principal/Main.java
@@ -0,0 +1,11 @@
+package paquete_principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ventana_Principal pruebas = new Ventana_Principal();
+		pruebas.setVisible(true);
+	}
+
+}
--- goHipster/src/paquete_principal/Ventana_Principal.java
+++ goHipster/src/paquete_principal/Ventana_Principal.java
@@ -68,22 +68,6 @@ public class Ventana_Principal extends JFrame {
 	private JScrollPane scrCaracteristicas;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Ventana_Principal frame = new Ventana_Principal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
 	public Ventana_Principal() {
