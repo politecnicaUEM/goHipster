f967c412e2fbbe5ab68e74aa02c162f45a33e4f8
21438255 <21438255@N204-05.alumnos.uem.es>
2015-10-27 12:21:54 +0100

    act file

--- goHipster/src/goHipster/MyFrame.java
+++ goHipster/src/goHipster/MyFrame.java
@@ -27,13 +27,32 @@ import javax.swing.JTextPane;
 
 import java.awt.Dimension;
 import java.awt.Color;
+
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
 import javax.swing.Box;
+
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.SwingConstants;
 
 public class MyFrame extends JFrame {
 
 	private JPanel contentPane;
-
+	private JCheckBox check1 = new JCheckBox("gafas pasta");
+	private JCheckBox check2 = new JCheckBox("barbita");
+	private JCheckBox check3 = new JCheckBox("tup\u00E9");
+	private JCheckBox check4 = new JCheckBox("bot\u00F3n cuello");
+	private JRadioButton radbut1 = new JRadioButton("hombre");
+	private JRadioButton radbut2 = new JRadioButton("mujer");
+	private JRadioButton radbut3 = new JRadioButton("otros");
+	private JTextArea Comentario = new JTextArea();
+	private JTextArea textArea = new JTextArea();
+	private JButton button1 = new JButton("");
+	private JButton button2 = new JButton("");
+	
 	/**
 	 * Launch the application.
 	 */
@@ -60,7 +79,7 @@ public class MyFrame extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(2, 2, 0, 2));
+		contentPane.setLayout(new GridLayout(2, 2, 2, 2));
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "features", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -73,17 +92,10 @@ public class MyFrame extends JFrame {
 		panel.add(panel_4);
 		panel_4.setLayout(new BoxLayout(panel_4, BoxLayout.Y_AXIS));
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("gafas pasta");
-		panel_4.add(chckbxNewCheckBox);
-		
-		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("barbita");
-		panel_4.add(chckbxNewCheckBox_1);
-		
-		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("tup\u00E9");
-		panel_4.add(chckbxNewCheckBox_2);
-		
-		JCheckBox chckbxNewCheckBox_3 = new JCheckBox("bot\u00F3n cuello");
-		panel_4.add(chckbxNewCheckBox_3);
+		panel_4.add(check1);
+		panel_4.add(check2);		
+		panel_4.add(check3);	
+		panel_4.add(check4);
 		
 		Component verticalGlue = Box.createVerticalGlue();
 		panel_4.add(verticalGlue);
@@ -102,18 +114,42 @@ public class MyFrame extends JFrame {
 		
 		ButtonGroup grupoBoton = new ButtonGroup();
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("hombre");
-		panel_7.add(rdbtnNewRadioButton);
+		panel_7.add(radbut1);		
+		radbut1.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				button1.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/Annoying-Hipster.png")));
+			}
+			
+		});
 		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("mujer");
-		panel_7.add(rdbtnNewRadioButton_1);
+		panel_7.add(radbut2);		
+		radbut2.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				button1.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/Female-User.png")));
+			}
+			
+		});
 		
-		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("otros");
-		panel_7.add(rdbtnNewRadioButton_2);
+		panel_7.add(radbut3);		
+		radbut3.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				button1.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/The-Batman.png")));
+			}
+			
+		});
 		
-		grupoBoton.add(rdbtnNewRadioButton);
-		grupoBoton.add(rdbtnNewRadioButton_1);
-		grupoBoton.add(rdbtnNewRadioButton_2);
+		grupoBoton.add(radbut1);
+		grupoBoton.add(radbut2);
+		grupoBoton.add(radbut3);
 		
 		Component verticalGlue_1 = Box.createVerticalGlue();
 		panel_7.add(verticalGlue_1);
@@ -125,17 +161,21 @@ public class MyFrame extends JFrame {
 		panel_5.add(panel_6);
 		panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.X_AXIS));
 		
-		JButton btnNewButton = new JButton("");
-		btnNewButton.setMargin(new Insets(1, 1, 1, 1));
-		panel_6.add(btnNewButton);
-		btnNewButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		btnNewButton.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/Annoying-Hipster.png")));
+		button1.setMargin(new Insets(1, 1, 1, 1));
+		panel_6.add(button1);
+		button1.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		button1.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/Annoying-Hipster.png")));
 		
-		JButton btnNewButton_1 = new JButton("");
-		btnNewButton_1.setMargin(new Insets(1, 1, 1, 1));
-		panel_6.add(btnNewButton_1);
-		btnNewButton_1.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnNewButton_1.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/Male-User.png")));
+		button1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				mostrar_eleccion();				
+			}
+		});
+		
+		button2.setMargin(new Insets(1, 1, 1, 1));
+		panel_6.add(button2);
+		button2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button2.setIcon(new ImageIcon(MyFrame.class.getResource("/icons/Male-User.png")));
 		
 		Component rigidArea = Box.createRigidArea(new Dimension(20, 20));
 		panel_5.add(rigidArea);
@@ -159,12 +199,28 @@ public class MyFrame extends JFrame {
 		JScrollPane scrollPane = new JScrollPane();
 		panel_2.add(scrollPane);
 		
-		JTextArea textArea = new JTextArea();
-		scrollPane.setViewportView(textArea);
+		Comentario.setText("Introduce un comentario");
+		scrollPane.setViewportView(Comentario);
 		
 		JPanel panel_3 = new JPanel();
 		panel_3.setBackground(Color.GRAY);
 		contentPane.add(panel_3);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		textArea.setLineWrap(true);
+		textArea.setCaretColor(Color.BLACK);
+		textArea.setBackground(Color.LIGHT_GRAY);
+		
+		panel_3.add(textArea);
+		
 	}
-
+	
+	public void mostrar_eleccion(){			
+		String txt = "", txt1 = "", txt2 = "", txt3 = "";		
+		if(check1.isSelected()){txt = check1.getText() + "\n";}	
+		if(check2.isSelected()){txt1 = check2.getText() + "\n";}		
+		if(check3.isSelected()){txt2 = check3.getText() + "\n";}		
+		if(check4.isSelected()){txt3 = check4.getText() + "\n";}		
+		textArea.setText(txt + txt1 + txt2 + txt3 + "\n" + Comentario.getText());	
+	}
+	
 }
