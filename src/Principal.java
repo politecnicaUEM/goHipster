1880f2a4f4f0171ef68a2669727967ee87913f9d
Roizus <alvaroroizo@gmail.com>
2015-10-30 17:57:34 +0100

    Interfaz GoHipster con eventos Álvaro Roizo

--- src/Principal.java
+++ src/Principal.java
@@ -10,6 +10,9 @@ import java.awt.Insets;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.border.TitledBorder;
+
+
+
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import java.awt.Color;
@@ -23,12 +26,44 @@ import java.awt.Component;
 import javax.swing.Box;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLayeredPane;
+import javax.swing.SwingConstants;
 
 public class Principal extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel panelSeleccion = new JPanel();
+	private JPanel panel_4 = new JPanel();
+	private JPanel panel_5 = new JPanel();
+	private JPanel panel_3 = new JPanel();
+	private JPanel panel_6 = new JPanel();
+	private JPanel panel_Tab2 = new JPanel();
+	private ButtonGroup group = new ButtonGroup();
+	private JCheckBox chckbxGafas = new JCheckBox();
+	private JCheckBox chckbxBarbas = new JCheckBox();
+	private JCheckBox chckbxTupe = new JCheckBox();
+	private JCheckBox chckbxCuelloBoton = new JCheckBox();
+	private JRadioButton rdbtnHombre = new JRadioButton();
+	private JRadioButton rdbtnMujer = new JRadioButton();
+	private JRadioButton rdbtnOtros = new JRadioButton();
+	private JTextArea textArea_1 = new JTextArea();
+	JTextArea txtrComentarios = new JTextArea();
+	JLabel lblCara = new JLabel();
+	JLabel labelBarba = new JLabel();
+	JLabel labelCuello = new JLabel();
+	JLabel labelGafas = new JLabel();
+	JLabel labelTupe = new JLabel();
+	JLabel lblQueTioTan = new JLabel();
+	JLabel lblCaranormal = new JLabel();
+	JLabel lblTextNormal = new JLabel();
+	
+	private final JLayeredPane layeredPane = new JLayeredPane();
+	private final JLayeredPane TabNoHipster = new JLayeredPane();
 
+	
 	/**
 	 * Launch the application.
 	 */
@@ -56,70 +91,75 @@ public class Principal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(2, 2, 0, 0));
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Features", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		contentPane.add(panel);
-		panel.setLayout(new GridLayout(1, 2, 0, 0));
 		
-		JPanel panel_4 = new JPanel();
-		panel.add(panel_4);
+		panelSeleccion.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Features", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		contentPane.add(panelSeleccion);
+		panelSeleccion.setLayout(new GridLayout(1, 2, 0, 0));
+		
+		
+		panelSeleccion.add(panel_4);
 		panel_4.setLayout(new BoxLayout(panel_4, BoxLayout.Y_AXIS));
 		
-		JCheckBox chckbxGafas = new JCheckBox("Gafa Pasta");
+		chckbxGafas = new JCheckBox("Gafa Pasta");
 		panel_4.add(chckbxGafas);
 		
-		JCheckBox chckbxBarbas = new JCheckBox("Barba");
+		chckbxBarbas = new JCheckBox("Barba");
 		panel_4.add(chckbxBarbas);
 		
-		JCheckBox chckbxTupe = new JCheckBox("Tupe");
+		chckbxTupe = new JCheckBox("Tupe");
 		panel_4.add(chckbxTupe);
 		
-		JCheckBox chckbxCuelloBoton = new JCheckBox("Boton Cuello");
+		chckbxCuelloBoton = new JCheckBox("Boton Cuello");
 		panel_4.add(chckbxCuelloBoton);
 		
-		JPanel panel_5 = new JPanel();
-		panel.add(panel_5);
+		panelSeleccion.add(panel_5);
 		panel_5.setLayout(new BoxLayout(panel_5, BoxLayout.Y_AXIS));
 		
-		JPanel panel_3 = new JPanel();
+		
 		panel_3.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		panel_5.add(panel_3);
 		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.Y_AXIS));
 		
-		ButtonGroup group = new ButtonGroup();
 		
-		JRadioButton rdbtnHombre = new JRadioButton("Hombre");
+		rdbtnHombre = new JRadioButton("Hombre");
 		panel_3.add(rdbtnHombre);
 		group.add(rdbtnHombre);
 		
-		JRadioButton rdbtnMujer = new JRadioButton("Mujer");
+		rdbtnMujer = new JRadioButton("Mujer");
 		panel_3.add(rdbtnMujer);
 		group.add(rdbtnMujer);
 		
-		JRadioButton rdbtnOtros = new JRadioButton("Otros");
+		rdbtnOtros = new JRadioButton("Otros");
 		panel_3.add(rdbtnOtros);
 		group.add(rdbtnOtros);
 		
 		Component verticalGlue = Box.createVerticalGlue();
-		panel_3.add(verticalGlue);
+		panel_5.add(verticalGlue);
 		
-		JPanel panel_6 = new JPanel();
+		
 		panel_5.add(panel_6);
 		panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.X_AXIS));
 		
-		JButton btnNewButton = new JButton("");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnButtonSinBarba = new JButton("");
+		btnButtonSinBarba.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textArea_1.setForeground(Color.WHITE);
+				textArea_1.setText(seleccionadosPanel1() + '\n'
+						+ txtrComentarios.getText());
+				int cont= seleccionadosParaCarita();
+				lblQueTioTan.setText(fraseSegunSeleccion(cont));
+				
 			}
 		});
-		btnNewButton.setIcon(new ImageIcon(Principal.class.getResource("/icon/Male-User.png")));
-		btnNewButton.setMargin(new Insets(0, 0, 0, 0));
+		btnButtonSinBarba.setIcon(new ImageIcon(Principal.class.getResource("/icon/Male-User.png")));
+		btnButtonSinBarba.setMargin(new Insets(0, 0, 0, 0));
 		
-		panel_6.add(btnNewButton);
+		panel_6.add(btnButtonSinBarba);
 		
 		JButton btnNewButton_1 = new JButton("");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textArea_1.setForeground(Color.LIGHT_GRAY);
 			}
 		});
 		btnNewButton_1.setIcon(new ImageIcon(Principal.class.getResource("/icon/Annoying-Hipster.png")));
@@ -127,32 +167,167 @@ public class Principal extends JFrame {
 		
 		panel_6.add(btnNewButton_1);
 		
-		JPanel panel_1 = new JPanel();
-		contentPane.add(panel_1);
-		panel_1.setLayout(new BorderLayout(0, 0));
+		JPanel panelTabsCaritas = new JPanel();
+		contentPane.add(panelTabsCaritas);
+		panelTabsCaritas.setLayout(new BorderLayout(0, 0));
 		
 		JTabbedPane TabPane = new JTabbedPane(JTabbedPane.TOP);
-		panel_1.add(TabPane, BorderLayout.CENTER);
-		JLabel label1 = new JLabel("To be a Hipster");
-		TabPane.addTab("Hipster", new ImageIcon(Principal.class.getResource("/icon/Annoying-Hipster.png")), label1, null);
-		JLabel label2 = new JLabel("Not to be a Hipster");
-		TabPane.addTab("No Hipster", new ImageIcon(Principal.class.getResource("/icon/Male-User.png")), label2, null);
+		panelTabsCaritas.add(TabPane, BorderLayout.CENTER);
 		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		contentPane.add(scrollPane_1);
+		TabPane.addTab("Hipster", new ImageIcon(Principal.class.getResource("/icon/Annoying-Hipster@Low.png")), layeredPane, null);
 		
-		JTextArea txtrAadeTusComentarios = new JTextArea();
-		txtrAadeTusComentarios.setText("Añade tus comentarios...");
-		scrollPane_1.setViewportView(txtrAadeTusComentarios);
+		lblCara = new JLabel("");
+		lblCara.setIcon(new ImageIcon(Principal.class.getResource("/icon_hipster/antes.png")));
+		lblCara.setBounds(93, 0, 99, 98);
+		layeredPane.add(lblCara);
+		lblCara.setVisible(true);
 		
+		labelBarba = new JLabel("");
+		labelBarba.setIcon(new ImageIcon(Principal.class.getResource("/icon_hipster/accesoriobarbita.png")));
+		labelBarba.setBounds(93, 0, 99, 98);
+		layeredPane.add(labelBarba);
+		labelBarba.setVisible(false);
 		
-		JPanel panel_2 = new JPanel();
-		contentPane.add(panel_2);
-		panel_2.setLayout(new BorderLayout(0, 0));
+		labelCuello = new JLabel("");
+		labelCuello.setIcon(new ImageIcon(Principal.class.getResource("/icon_hipster/accesoriobotoncuello.png")));
+		labelCuello.setBounds(93, 0, 99, 98);
+		layeredPane.add(labelCuello);
+		labelCuello.setVisible(false);
 		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBackground(Color.GRAY);
-		panel_2.add(textArea_1, BorderLayout.CENTER);
-	}
+		labelGafas = new JLabel("");
+		labelGafas.setIcon(new ImageIcon(Principal.class.getResource("/icon_hipster/accesoriogafapasta.png")));
+		labelGafas.setBounds(93, 0, 99, 98);
+		layeredPane.add(labelGafas);
+		labelGafas.setVisible(false);
+		
+		labelTupe = new JLabel("");
+		labelTupe.setIcon(new ImageIcon(Principal.class.getResource("/icon_hipster/accesoriotupe.png")));
+		labelTupe.setBounds(93, 0, 99, 98);
+		layeredPane.add(labelTupe);
+		labelTupe.setVisible(false);
+		
+		lblQueTioTan = new JLabel("Dale al boton y veremos si eres hipster");
+		lblQueTioTan.setHorizontalAlignment(SwingConstants.CENTER);
+		lblQueTioTan.setBounds(6, 103, 287, 16);
+		layeredPane.add(lblQueTioTan);
+		lblQueTioTan.setVisible(true);
+		
+		lblCaranormal = new JLabel("");
+		lblCaranormal.setIcon(new ImageIcon(Principal.class.getResource("/icon/Male-User@High.png")));
+		lblCaranormal.setBounds(93, 0, 99, 98);
+		TabNoHipster.add(lblCaranormal);
+		lblCaranormal.setVisible(true);
+		
+		lblTextNormal = new JLabel("Así serías si dejas el rollo hipster");
+		lblTextNormal.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTextNormal.setBounds(6, 103, 287, 16);
+		TabNoHipster.add(lblTextNormal);
+		lblTextNormal.setVisible(true);
+		
+		TabPane.addTab("No Hipster", new ImageIcon(Principal.class.getResource("/icon/Male-User@Low.png")), TabNoHipster, null);
+		
+		JScrollPane scrollTextoComentarios = new JScrollPane();
+		contentPane.add(scrollTextoComentarios);
+		
+		txtrComentarios.setText("Añade tus comentarios...");
+		txtrComentarios.addFocusListener(new FocusListener(){
 
+			@Override
+			public void focusGained(FocusEvent e) {
+				// TODO Auto-generated method stub
+				txtrComentarios.setText("");
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				// TODO Auto-generated method stub
+				
+			}});
+		
+		scrollTextoComentarios.setViewportView(txtrComentarios);
+		
+		
+		JPanel panelResultado = new JPanel();
+		contentPane.add(panelResultado);
+		panelResultado.setLayout(new BorderLayout(0, 0));
+		textArea_1.setEditable(false);
+		
+		textArea_1.setBackground(Color.GRAY);
+		panelResultado.add(textArea_1, BorderLayout.CENTER);
+	}
+	
+	public String seleccionadosPanel1() {
+		String text = "";
+		labelGafas.setVisible(false);
+		labelBarba.setVisible(false);
+		labelTupe.setVisible(false);
+		labelCuello.setVisible(false);
+		int cont = 0;
+		if (chckbxGafas.isSelected()){
+			text += "Llevas gafitas\n";
+		}
+		if (chckbxBarbas.isSelected()){
+			text += "Llevas barbita\n";
+		}
+		if (chckbxTupe.isSelected()){
+			text += "Llevas tupe\n";
+		}
+		if (chckbxCuelloBoton.isSelected()){
+			text += "Llevas el botón en el cuello de moda\n";
+		}
+		if (rdbtnMujer.isSelected()){
+			text += "Eres una mujer\n";
+		}
+		if (rdbtnHombre.isSelected()){
+			text += "Eres un hombre\n";
+		}
+		if (rdbtnOtros.isSelected()){
+			text += "Mmmm menuda sexualidad diversa\n";
+		}
+		return text;
+	}
+	public int seleccionadosParaCarita() {
+		labelGafas.setVisible(false);
+		labelBarba.setVisible(false);
+		labelTupe.setVisible(false);
+		labelCuello.setVisible(false);
+		int cont = 0;
+		if (chckbxGafas.isSelected()){
+			labelGafas.setVisible(true);
+			cont +=1;
+		}
+		if (chckbxBarbas.isSelected()){
+			labelBarba.setVisible(true);
+			cont +=1;
+		}
+		if (chckbxTupe.isSelected()){
+			labelTupe.setVisible(true);
+			cont +=1;
+		}
+		if (chckbxCuelloBoton.isSelected()){
+			labelCuello.setVisible(true);
+			cont +=1;
+		}
+		return cont;
+	}
+	
+	public String fraseSegunSeleccion(int cont){
+		String text = "";
+		if(cont==0){
+			text = "No eres ni un intento de Hipster";
+		}
+		else if(cont==1){
+			text = "Te crees hipster pero no!";
+		}
+		else if(cont==2){
+			text = "No eres tan hipster como te piensas";
+		}
+		else if(cont==3){
+			text = "Eres un hipster del montón";
+		}
+		else if(cont==4){
+			text = "Eres un tio muy hipster";
+		}
+		return text;
+	}
 }
