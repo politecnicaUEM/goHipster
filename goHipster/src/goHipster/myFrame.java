cd466ffcefa9a6c6b0aa0977e18dec579d8c154a
21438659 <21438659@N204-09.alumnos.uem.es>
2015-11-03 09:02:25 +0100

    Finish

--- goHipster/src/goHipster/myFrame.java
+++ goHipster/src/goHipster/myFrame.java
@@ -1,43 +1,44 @@
 package goHipster;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import java.awt.GridLayout;
-
-import javax.swing.ImageIcon;
-import javax.swing.JToggleButton;
-import javax.swing.BoxLayout;
-import javax.swing.JCheckBox;
-import javax.swing.JRadioButton;
-import javax.swing.JTextArea;
-
 import java.awt.Color;
-
-import javax.swing.JTabbedPane;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-
-
-
-
-
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Component;
-import javax.swing.border.LineBorder;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 
 public class myFrame extends JFrame {
 
 	private JPanel contentPane;
-
+	private JCheckBox chckbxGafasPasta;
+	private JCheckBox chckbxBarbita;
+	private JCheckBox chckbxCuelloAbotonado;
+	private JCheckBox chckbxTup;
+	private JRadioButton rdbtnGafasPasta;
+	private JRadioButton rdbtnBarba;
+	private JRadioButton rdbtnTup;
+	private JTextArea textArea;
+	private JTextArea textArea_1;
+	private JButton btnNewButton;
+	private JRadioButton rdbtnNewRadioButton;
+	private JRadioButton rdbtnNewRadioButton_1;
+	private JRadioButton rdbtnNewRadioButton_2;
 	/**
 	 * Launch the application.
 	 */
@@ -75,19 +76,19 @@ public class myFrame extends JFrame {
 		panel.add(panel_4);
 		panel_4.setLayout(new BoxLayout(panel_4, BoxLayout.Y_AXIS));
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("gafas pasta");
-		chckbxNewCheckBox.setSelected(true);
-		panel_4.add(chckbxNewCheckBox);
+		chckbxGafasPasta = new JCheckBox("gafas pasta");
+		chckbxGafasPasta.setSelected(true);
+		panel_4.add(chckbxGafasPasta);
 		
-		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("barbita");
-		chckbxNewCheckBox_1.setSelected(true);
-		panel_4.add(chckbxNewCheckBox_1);
+		chckbxBarbita = new JCheckBox("barbita");
+		chckbxBarbita.setSelected(true);
+		panel_4.add(chckbxBarbita);
 		
-		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("tup\u00E8");
-		panel_4.add(chckbxNewCheckBox_2);
+		chckbxTup= new JCheckBox("tup\u00E8");
+		panel_4.add(chckbxTup);
 		
-		JCheckBox chckbxNewCheckBox_3 = new JCheckBox("bot\u00F2n cuello");
-		panel_4.add(chckbxNewCheckBox_3);
+		chckbxCuelloAbotonado = new JCheckBox("bot\u00F2n cuello");
+		panel_4.add(chckbxCuelloAbotonado);
 		
 		Component verticalGlue = Box.createVerticalGlue();
 		panel_4.add(verticalGlue);
@@ -96,14 +97,14 @@ public class myFrame extends JFrame {
 		panel.add(panel_5);
 		panel_5.setLayout(new BoxLayout(panel_5, BoxLayout.Y_AXIS));
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("hombre");
+		rdbtnNewRadioButton = new JRadioButton("hombre");
 		panel_5.add(rdbtnNewRadioButton);
 		
-		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("mujer");
+		rdbtnNewRadioButton_2 = new JRadioButton("mujer");
 		rdbtnNewRadioButton_2.setSelected(true);
 		panel_5.add(rdbtnNewRadioButton_2);
 		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("otros");
+		rdbtnNewRadioButton_1 = new JRadioButton("otros");
 		panel_5.add(rdbtnNewRadioButton_1);
 		
 		Component verticalGlue_1 = Box.createVerticalGlue();
@@ -112,9 +113,12 @@ public class myFrame extends JFrame {
 		JPanel panel_6 = new JPanel();
 		panel_5.add(panel_6);
 		
-		JButton btnNewButton = new JButton();
+		btnNewButton = new JButton();
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				textArea_1.setForeground(Color.WHITE);
+				textArea_1.setText(selection2Pane() + '\n'
+						+ textArea.getText());
 			}
 		});
 		panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.X_AXIS));
@@ -150,16 +154,36 @@ public class myFrame extends JFrame {
 		contentPane.add(panel_2);
 		panel_2.setLayout(new BoxLayout(panel_2, BoxLayout.X_AXIS));
 		
-		JTextArea textArea = new JTextArea();
+		 textArea = new JTextArea();
 		panel_2.add(textArea);
 		
 		JPanel panel_3 = new JPanel();
 		contentPane.add(panel_3);
 		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
 		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBackground(Color.GRAY);
-		panel_3.add(textArea_1);
+		textArea_1 = new JTextArea();
+		textArea_1.setEditable(false);
+		textArea_1.setBackground(new Color(120, 120, 120, 255));
+		textArea_1.setForeground(Color.WHITE);
+		panel_3.add(textArea_1, BorderLayout.CENTER);
 	}
+	public String selection2Pane() {
+		String text = "";
+		if (chckbxGafasPasta.isSelected())
+			text += "con sus gafitas\n";
+		if (chckbxBarbita.isSelected())
+			text += "con su barbita\n";
+		if (chckbxTup.isSelected())
+			text += "con su tupe\n";
+		if (chckbxCuelloAbotonado.isSelected())
+			text += "con su botón en el cuello de moda\n";
+		if (rdbtnNewRadioButton_2.isSelected())
+			text += "esa mujer\n";
+		if (rdbtnNewRadioButton.isSelected())
+			text += "ese hombre\n";
+		if (rdbtnNewRadioButton_1.isSelected())
+			text += "esa sexualidad diversa\n";
+		return text;
 
+	}
 }
